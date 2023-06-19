76f080b4017bf4ba15f09bfd05d64d7bb6a7a4de
A Stone <astone@heckgrammar.co.uk>
2023-06-19 13:43:38 +0100

    Add project files.

---
+++ 3.2.4-RelationalOperations-VB/Module1.vb
@@ -0,0 +1,38 @@
+﻿Module Module1
+
+    Sub Main()
+        ' Equal to
+        Dim a As Integer = 1
+        If a = 1 Then
+            Console.WriteLine("Equal to")
+        End If
+
+        ' Not equal to
+        If a <> 1 Then
+            Console.WriteLine("Not equal to")
+        End If
+
+        ' less than
+        If a < 1 Then
+            Console.WriteLine("Less than")
+        End If
+
+        ' greater than
+        If a > 1 Then
+            Console.WriteLine("Greater than")
+        End If
+
+        ' less than or equal to
+        If a <= 1 Then
+            Console.WriteLine("Less than or equal to")
+        End If
+
+        ' greater than or equal to
+        If a >= 1 Then
+            Console.WriteLine("Greater than or equal to")
+        End If
+
+
+    End Sub
+
+End Module
